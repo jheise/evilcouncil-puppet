740a9c256d651d2fcf901b263804c6e934f0e997
Jon Heise <j.heise@gmail.com>
2016-12-11 16:22:36 -0500

    adding tracy

--- modules/accounts/manifests/init.pp
+++ modules/accounts/manifests/init.pp
@@ -1,8 +1,8 @@
 # include all active accounts in here
 class accounts {
     include accounts::jubei
+    include accounts::tracy
     if $others {
         include accounts::chestone
-        include accounts::tracy
     }
 }
