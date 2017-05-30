e327a020553a71aa09b87d16e71ad1d44a433add
Jon Heise <j.heise@gmail.com>
2017-05-29 22:07:53 -0700

    commenting out ed25519 key

--- modules/accounts/manifests/jubei.pp
+++ modules/accounts/manifests/jubei.pp
@@ -81,19 +81,20 @@ class accounts::jubei {
         key => "AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAA/Vjz+HSy4E/Tchv2Pz0q+jBT5vqBRJPXluf/yoUtbmHM9U/MXSaVCl/LihIYTItNYXfGv/1A7XS7uQWF9FsgBqgFP6co17kOMBYRx9BqMOqYEr3DZqZ5YS6cbBHhIUBHXtruoP9PvDxImV4H9CyarvH3aT41ijFPTkBZLv6kPGr6uJw=="
     }
 
-    ssh_authorized_key { "wintermute":
-        ensure => present,
-        type => "ssh-ed25519",
-        user => "jubei",
-        key => "AAAAC3NzaC1lZDI1NTE5AAAAILsdoFS4jbXziQcc0aNjdumK8BgCgf8m1B9sgY97Mk6f",
-	require => File["/home/jubei/.ssh"]
-    }
+    #ssh_authorized_key { "wintermute":
+    #    ensure => present,
+    #    type => "ssh-ed25519",
+    #    user => "jubei",
+    #    key => "AAAAC3NzaC1lZDI1NTE5AAAAILsdoFS4jbXziQcc0aNjdumK8BgCgf8m1B9sgY97Mk6f",
+	#require => File["/home/jubei/.ssh"]
+    #}
 
     ssh_authorized_key { "wintermute-rsa":
         ensure => present,
         type => "ssh-rsa",
         user => "jubei",
         key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC0O5MWLRslh9ectEfnqN76v+ex0yaTpDEcp+fNPcZRWXzkbiYT5Z3IjSdG4xuL62ZnX6uPeudcJDfFp8TFOEu6RWDimACzWp96inVILBq7S5PP7+v0KQ/X3KEamy5RpcQLYMKGieLPLDuCLv9mRlUFI13WLJFO1vq9rojl9/c+A1Ocu+ENO8dYS7mzSb29Ib3n68uHC3vbLrpfwzHhHKqj2VP8ds4IAuINabK/kG1NEPUAoP5IQtIG0croa3BXaUmgNlvHIcFRNozo7SRsOhcrq/dUHvDh580+KARUp4x2EWDQ8bvI2AANAtYcaIils6nXy2VDEcTzUkcwl08byfYj"
+        require => File["/home/jubei/.ssh"]
     }
 
     ssh_authorized_key { "work":
